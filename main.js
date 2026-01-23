c4452735206435c007d436258a152c08d3c7a3b1
varbris <92345143+Varbris@users.noreply.github.com>
2026-01-23 09:20:33 +0700

    feat: hashmap set method

---
+++ main.js
@@ -0,0 +1,43 @@
+import { LinkedList } from "./LinkedList.js";
+
+class HashMap {
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.buckets = new Array(capacity);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+    return hashCode;
+  }
+
+  set(key, value) {
+    if (typeof key !== "string") {
+      try {
+        throw new Error("key type Error, use string instead");
+      } catch (err) {
+        console.log(err.message);
+      }
+      return;
+    }
+    const index = this.hash(key);
+    if (this.buckets[index % this.buckets.length] == undefined) {
+      this.buckets[index % this.buckets.length] = { key: key, value: value };
+    } else if (this.buckets[index % this.buckets.length].key !== key) {
+      const previous_data = this.buckets[index % this.buckets.length];
+      const list = new LinkedList();
+      list.append(previous_data);
+      list.append({ key, value });
+    }
+  }
+}
+
+const myHashMap = new HashMap();
+myHashMap.set("Rama", "awokawok");
+myHashMap.set("Sita", "awokawoks");
